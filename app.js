a02376bad42dfa6363f9aa51ffc546ce58038ca0
Eloo <eloblogs@gmail.com>
2022-05-13 20:52:43 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Elochukwu. This is your first Edit.'));
+app.get('/', (req, res) => res.send('Hello Elochukwu. This is your second Edit for a new commit that will pushed to the repo.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
